f59096fd176aa157be2a03817ac9e8807ee4c77c
aluvanze <77957856+aluvanze@users.noreply.github.com>
2022-09-10 17:21:05 +0300

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ print("*" *28)
 print("*TWO number Basic calculator*")
 print("*" *28)
 first_number= input("  Input your first number \n ------>")
+operator=input
 second_number= input("Input your second number \n ------>")
 answer=int(first_number)-int(second_number)
 print("*"*28)
